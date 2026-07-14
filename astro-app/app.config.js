b488328c948d0e4c1cd7fd596850ba6a55fb34b0
sunainasharma25082004-lang <sunainasharma25082004@gmail.com>
2026-07-14 11:39:48 +0530

    edited

--- astro-app/app.config.js
+++ astro-app/app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: 'AstroTalk Partner',
     slug: 'astrotalk-astro',
-     "owner": "sunaina0s-team",
+    owner: 'sunaina0s-team',
     version: '1.0.0',
     platforms: ['android', 'ios'],
     orientation: 'portrait',
